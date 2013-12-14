ce762d36f67aafff853bcc59c0eaf1a0d7e7e9d4
HiromiShikata <shikata.hiromi@gmail.com>
2013-12-15 02:29:15 +0900

    firstcommit

---
+++ src/com/imorih/kcmjp106changealtctrl/InputDeviceReceiver.java
@@ -0,0 +1,12 @@
+package com.imorih.kcmjp106changealtctrl;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class InputDeviceReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context arg0, Intent arg1) {
+	}
+}
